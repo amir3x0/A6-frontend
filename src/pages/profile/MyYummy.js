d75a7565863fe05f38f1263f2f2c899c7c826eae
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2024-02-27 19:53:29 +0200

    נ

--- src/pages/profile/MyYummy.js
+++ src/pages/profile/MyYummy.js
@@ -12,3 +12,7 @@ const MyYummy = () => {
 };
 
 export default MyYummy;
+
+{/* <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+sign in
+</button> */}
