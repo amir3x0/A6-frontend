b3e4a722a6a7dc2aa08ee9f8b38eb5e760e15e29
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2024-03-02 15:55:30 +0200

    lital search fix

--- src/components/RecipeSection.js
+++ src/components/RecipeSection.js
@@ -2,50 +2,174 @@ import React, { useState, useEffect } from "react";
 import RecipeCard from './RecipeCard'; // Make sure the path is correct
 
 
-// Assuming the recipes are categorized accordingly
+const images = {
+  Bruschetta: process.env.PUBLIC_URL + "/images/recipe_img/Bruschetta.jpg",
+  Caprese_Salad: process.env.PUBLIC_URL + "/images/recipe_img/Caprese_Salad.jpg",
+  Cheese_Platter:process.env.PUBLIC_URL + "/images/recipe_img/Cheese_Platter.jpg",
+  Artichoke: process.env.PUBLIC_URL + "/images/recipe_img/Artichoke.jpg",
+  Mushrooms:process.env.PUBLIC_URL + "/images/recipe_img/Mushrooms.jpg",
+  Spring_Rolls:process.env.PUBLIC_URL + "/images/recipe_img/Spring_Rolls.jpg",
+  filled_pepper:process.env.PUBLIC_URL + "/images/recipe_img/filled_pepper.jpg",
+  hamburger:process.env.PUBLIC_URL + "/images/recipe_img/hamburger.jpg",
+  meatballs:process.env.PUBLIC_URL + "/images/recipe_img/meatballs.jpg",
+  cheese_cake:process.env.PUBLIC_URL + "/images/recipe_img/cheese_cake.jpg",
+  fruitcake:process.env.PUBLIC_URL + "/images/recipe_img/fruitcake.jpg",
+  sufle:process.env.PUBLIC_URL + "/images/recipe_img/sufle.jpg",
+  // Add more image paths if needed for other recipes
+};
+
 const categorizedRecipes = {
   appetizers: [
     {
-      name: "Bruschetta",
-      image: process.env.PUBLIC_URL + "/images/recipe_img/bruschetta.jpg",
+      id: 1,
+      title: "Bruschetta",
+      description: "A classic Italian appetizer with tomatoes, basil, and mozzarella.",
       ingredients: ["Ingredient A", "Ingredient B"],
+      instructions: ["Step 1: Slice tomatoes.", "Step 2: Arrange basil leaves.", "Step 3: Add mozzarella slices."],
+      difficulty: "Easy",
+      rank: 5,
+      categories: ["appetizers", "Bruschetta"],
+      picture: images.Bruschetta,
+      calories: { total: 500, protein: "30g", carbs: "35g", fat: "22g" },
     },
-    // Add more appetizer recipes
+    {
+      id: 2,
+      title: "Caprese Salad",
+      description: "A refreshing salad with tomatoes, mozzarella, and balsamic glaze.",
+      ingredients: ["Ingredient A", "Ingredient B", "Ingredient C"],
+      instructions: ["Step 1: Slice tomatoes and mozzarella.", "Step 2: Arrange on a plate.", "Step 3: Drizzle with balsamic glaze."],
+      difficulty: "Easy",
+      rank: 4,
+      categories: ["appetizers", "Caprese Salad"],
+      picture: images.Caprese_Salad,
+      calories: { total: 400, protein: "20g", carbs: "25g", fat: "18g" },
+    },
+    {
+      id: 3,
+      title: "Cheese Platter",
+      description: "An assorted cheese platter with crackers and fruits.",
+      ingredients: ["Ingredient A", "Ingredient B", "Ingredient C"],
+      instructions: ["Step 1: Arrange a variety of cheeses.", "Step 2: Add crackers and fruits.", "Step 3: Serve and enjoy."],
+      difficulty: "Easy",
+      rank: 4,
+      categories: ["appetizers", "Cheese Platter"],
+      picture: images.Cheese_Platter,
+      calories: { total: 450, protein: "25g", carbs: "30g", fat: "20g" },
+    },
   ],
   starters: [
-    {
-      name: "Caesar Salad",
-      image: process.env.PUBLIC_URL + "/images/recipe_img/caesar_salad.jpg",
-      ingredients: ["Ingredient A", "Ingredient B"],
-    },
-    // Add more starter recipes
+      {
+        id: 4,
+        title: "Artichoke",
+        description: "A classic Italian appetizer with tomatoes, basil, and mozzarella.",
+        ingredients: ["Ingredient A", "Ingredient B"],
+        instructions: ["Step 1: Slice tomatoes.", "Step 2: Arrange basil leaves.", "Step 3: Add mozzarella slices."],
+        difficulty: "Easy",
+        rank: 5,
+        categories: ["starters", "Artichoke"],
+        picture: images.Artichoke,
+        calories: { total: 500, protein: "30g", carbs: "35g", fat: "22g" },
+      },
+      {
+        id: 5,
+        title: " Mushrooms",
+        description: "A refreshing salad with tomatoes, mozzarella, and balsamic glaze.",
+        ingredients: ["Ingredient A", "Ingredient B", "Ingredient C"],
+        instructions: ["Step 1: Slice tomatoes and mozzarella.", "Step 2: Arrange on a plate.", "Step 3: Drizzle with balsamic glaze."],
+        difficulty: "Easy",
+        rank: 4,
+        categories: ["starters", " Mushrooms"],
+        picture: images. Mushrooms,
+        calories: { total: 400, protein: "20g", carbs: "25g", fat: "18g" },
+      },
+      {
+        id: 6,
+        title: "Spring Rolls",
+        description: "An assorted cheese platter with crackers and fruits.",
+        ingredients: ["Ingredient A", "Ingredient B", "Ingredient C"],
+        instructions: ["Step 1: Arrange a variety of cheeses.", "Step 2: Add crackers and fruits.", "Step 3: Serve and enjoy."],
+        difficulty: "Easy",
+        rank: 4,
+        categories: ["starters", "Spring_Rolls"],
+        picture: images.Spring_Rolls,
+        calories: { total: 450, protein: "25g", carbs: "30g", fat: "20g" },
+      },
   ],
   mainDishes: [
-    // Include your initial recipes here
     {
-      name: "Filled pepper",
-      image: process.env.PUBLIC_URL + "/images/recipe_img/filled_pepper.jpg",
-      ingredients: ["4 large bell peppers", "1 lb beef", "chopped onion"],
+      id: 7,
+      title: "filled_pepper",
+      description: "A classic Italian appetizer with tomatoes, basil, and mozzarella.",
+      ingredients: ["Ingredient A", "Ingredient B"],
+      instructions: ["Step 1: Slice tomatoes.", "Step 2: Arrange basil leaves.", "Step 3: Add mozzarella slices."],
+      difficulty: "Easy",
+      rank: 5,
+      categories: ["mainDishes", "filled pepper"],
+      picture: images.filled_pepper,
+      calories: { total: 500, protein: "30g", carbs: "35g", fat: "22g" },
     },
     {
-      name: "Meatballs",
-      image: process.env.PUBLIC_URL + "/images/recipe_img/meatballs.jpg",
+      id: 8,
+      title: "hamburger",
+      description: "A refreshing salad with tomatoes, mozzarella, and balsamic glaze.",
       ingredients: ["Ingredient A", "Ingredient B", "Ingredient C"],
+      instructions: ["Step 1: Slice tomatoes and mozzarella.", "Step 2: Arrange on a plate.", "Step 3: Drizzle with balsamic glaze."],
+      difficulty: "Easy",
+      rank: 4,
+      categories: ["mainDishes", "hamburger"],
+      picture: images. hamburger,
+      calories: { total: 400, protein: "20g", carbs: "25g", fat: "18g" },
     },
     {
-      name: "Soup",
-      image: process.env.PUBLIC_URL + "/images/recipe_img/soup.jpg",
+      id: 9,
+      title: "meatballs",
+      description: "An assorted cheese platter with crackers and fruits.",
       ingredients: ["Ingredient A", "Ingredient B", "Ingredient C"],
+      instructions: ["Step 1: Arrange a variety of cheeses.", "Step 2: Add crackers and fruits.", "Step 3: Serve and enjoy."],
+      difficulty: "Easy",
+      rank: 4,
+      categories: ["mainDishes", "meatballs"],
+      picture: images. meatballs,
+      calories: { total: 450, protein: "25g", carbs: "30g", fat: "20g" },
     },
-    // Add more main dish recipes
   ],
   desserts: [
     {
-      name: "Tiramisu",
-      image: process.env.PUBLIC_URL + "/images/recipe_img/tiramisu.jpg",
+      id: 10,
+      title: "cheese cake",
+      description: "A classic Italian appetizer with tomatoes, basil, and mozzarella.",
       ingredients: ["Ingredient A", "Ingredient B"],
+      instructions: ["Step 1: Slice tomatoes.", "Step 2: Arrange basil leaves.", "Step 3: Add mozzarella slices."],
+      difficulty: "Easy",
+      rank: 5,
+      categories: ["desserts", "cheese cake"],
+      picture: images.cheese_cake,
+      calories: { total: 500, protein: "30g", carbs: "35g", fat: "22g" },
     },
-    // Add more dessert recipes
+    {
+      id: 11,
+      title: "fruit cake",
+      description: "A refreshing salad with tomatoes, mozzarella, and balsamic glaze.",
+      ingredients: ["Ingredient A", "Ingredient B", "Ingredient C"],
+      instructions: ["Step 1: Slice tomatoes and mozzarella.", "Step 2: Arrange on a plate.", "Step 3: Drizzle with balsamic glaze."],
+      difficulty: "Easy",
+      rank: 4,
+      categories: ["desserts", "fruit cake"],
+      picture: images. fruitcake,
+      calories: { total: 400, protein: "20g", carbs: "25g", fat: "18g" },
+    },
+    {
+      id:12,
+      title: "sufle",
+      description: "An assorted cheese platter with crackers and fruits.",
+      ingredients: ["Ingredient A", "Ingredient B", "Ingredient C"],
+      instructions: ["Step 1: Arrange a variety of cheeses.", "Step 2: Add crackers and fruits.", "Step 3: Serve and enjoy."],
+      difficulty: "Easy",
+      rank: 4,
+      categories: ["desserts", "sufle"],
+      picture: images. sufle,
+      calories: { total: 450, protein: "25g", carbs: "30g", fat: "20g" },
+    },
   ],
 };
 
@@ -60,15 +184,13 @@ export default function RecipeSection() {
       return;
     }
 
-    const newFiltered = Object.keys(categorizedRecipes).reduce(
-      (acc, category) => {
-        acc[category] = categorizedRecipes[category].filter((recipe) =>
-          recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        return acc;
-      },
-      {}
-    );
+    const newFiltered = Object.keys(categorizedRecipes).reduce((acc, category) => {
+      acc[category] = categorizedRecipes[category].filter((recipe) =>
+        recipe.name?.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      return acc;
+    }, {});
+    
 
     setFilteredRecipes(newFiltered);
   }, [searchTerm]);
@@ -77,7 +199,14 @@ export default function RecipeSection() {
     setSelectedRecipe(selectedRecipe === recipe ? null : recipe);
   };
 
+
   document.title = "Our Recipes";
+  const categoryColors = {
+    appetizers: "text-background-color: #FFB6C1; color: #000000;", // LightPink background with Black text
+    starters: "background-color: #ADD8E6; color: #000000;", // LightBlue background with Black text
+    mainDishes: "background-color: #90EE90; color: #000000;", // LightGreen background with Black text
+    desserts: "background-color: #FFFACD; color: #000000;", // LemonChiffon background with Black text
+  };
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -102,7 +231,9 @@ export default function RecipeSection() {
 
       {Object.entries(filteredRecipes).map(([category, recipes]) => (
         <div key={category} className="mb-12">
-          <h2 className="text-2xl font-bold mb-4 capitalize">{category}</h2>
+          <h2 className={`text-2xl font-bold mb-4 capitalize ${categoryColors[category]}`}>
+            {category}
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {recipes.map((recipe, index) => (
               <div
